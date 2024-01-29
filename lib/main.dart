4d7ae8c404ea2d440dc221a9d2f9521703465b5a
sachin <118202383+kghSachin@users.noreply.github.com>
2024-01-29 23:26:49 +0545

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,3 @@ class MyApp extends StatelessWidget {
         home: const SignUpPage());
   }
 }
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
